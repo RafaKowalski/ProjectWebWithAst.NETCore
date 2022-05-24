65515e33d9a0f5b585700a923f8f924176173fe5
Rafael Kowalski <rafaelmk06@gmail.com>
2022-05-24 20:07:19 -0300

    First controller and Razor pages testes

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Testando";
+            ViewData["Aluno"] = "Rafael Kowalski";
 
             return View();
         }
